f7d6f84a9ac6d1860fe390514aa976adcf221812
rekoe <koukou890@qq.com>
2014-08-31 07:37:02 +0800

    Modules 映射

--- src/main/java/cn/xuetang/modules/MainModule.java
+++ src/main/java/cn/xuetang/modules/MainModule.java
@@ -1,26 +1,31 @@
 package cn.xuetang.modules;
 
-import cn.xuetang.core.UrlMappingSet;
-import org.nutz.mvc.annotation.*;
+import org.nutz.mvc.annotation.By;
+import org.nutz.mvc.annotation.Encoding;
+import org.nutz.mvc.annotation.Fail;
+import org.nutz.mvc.annotation.Filters;
+import org.nutz.mvc.annotation.IocBy;
+import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.annotation.SetupBy;
 import org.nutz.mvc.ioc.provider.ComboIocProvider;
 
+import cn.xuetang.common.filter.ShiroActionFilter;
 import cn.xuetang.core.StartSetup;
 
-/** 
- * 类描述： 
- * 创建人：Wizzer 
- * 联系方式：www.wizzer.cn
- * 创建时间：2013-11-26 下午2:08:37 
- * @version 
+/**
+ * 类描述： 创建人：Wizzer 联系方式：www.wizzer.cn 创建时间：2013-11-26 下午2:08:37
+ * 
+ * @version
  */
 
-@Modules(scanPackage=true)
+@Modules(scanPackage = true)
 @Ok("raw")
 @Fail("http:500")
-@IocBy(type=ComboIocProvider.class,args={
-	"*org.nutz.ioc.loader.json.JsonLoader","config",
-	"*org.nutz.ioc.loader.annotation.AnnotationIocLoader","cn.xuetang"})
-@SetupBy(value=StartSetup.class)
-@UrlMappingBy(value=UrlMappingSet.class)
+@IocBy(type = ComboIocProvider.class, args = { "*org.nutz.ioc.loader.json.JsonLoader", "config", "*org.nutz.ioc.loader.annotation.AnnotationIocLoader", "cn.xuetang" })
+@SetupBy(value = StartSetup.class)
+@Filters(@By(type = ShiroActionFilter.class, args = "/login.jsp"))
+@Encoding(input = "UTF-8", output = "UTF-8")
+// @UrlMappingBy(value=UrlMappingSet.class)
 public class MainModule {
 }
